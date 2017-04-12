f00ca2986a2caf328eaae27b2fbe3fabc8daefe6
Nuurek <nurskijakub@gmail.com>
2017-04-13 01:14:47 +0200

    First attempt to merge layout.

--- iotodolists/settings.py
+++ iotodolists/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'iotodolists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,4 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
