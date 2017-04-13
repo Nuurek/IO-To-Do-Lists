704a3bd568b75d70397344cb1a461475a94a82eb
Krzysztof Tomczak <krzysztof.g.tomczak@student.put.poznan.pl>
2017-04-13 09:15:17 +0200

    Added missing documentation in model and views.

--- superlists/models.py
+++ superlists/models.py
@@ -3,14 +3,16 @@ from django.utils import timezone
 
 # Create your models here.
 
-'''
-The ToDoList class defines the main storage unit in application.
-Each list has three fields:
-name - name of the list defined by user
-creation_date - determines when the list was created
-is_private - does user allow for displaying his/her list on main page?
-'''
+
 class ToDoList(models.Model):
+    '''
+    The ToDoList class defines the main storage unit in application.
+
+    Attributes:
+        name - name of the list defined by user
+        creation_date - determines when the list was created
+        is_private - does user allow for displaying his/her list on main page
+    '''
     name = models.CharField(max_length=200)
     creation_date = models.DateTimeField(
         'date created', auto_now_add=True, blank=True)
@@ -21,6 +23,14 @@ class ToDoList(models.Model):
 
 
 class ToDoListItem(models.Model):
+    '''
+    Represents a single task on a todo list.
+
+    Attributes:
+        name - title of the item
+        completed - true if task is marked as completed, false otherwise
+        todo_list - ToDoList corresponding to this item
+    '''
     name = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
--- superlists/views.py
+++ superlists/views.py
@@ -8,8 +8,16 @@ from .forms import ToDoListCreationForm, ToDoListItemAdditionForm
 
 # Create your views here.
 
-
 def index(request):
+    '''
+    Handles HTTP request for the main page.
+
+    Args:
+        request - HTTP request
+
+    Returns:
+        HTTP response with the main page view.
+    '''
     latest_todo_lists = ToDoList.objects.order_by('-creation_date')[:10]
     template = loader.get_template('superlists/index.html')
     form = ToDoListCreationForm()
@@ -21,6 +29,16 @@ def index(request):
 
 
 def detail(request, todo_list_id):
+    '''
+    Handles HTTP request for the detailed view of a list.
+
+    Args:
+        request - HTTP request
+        todo_list_id - id of a list to display
+
+    Returns:
+        HTTP response with list details view if list exists, 404 error code page otherwise.
+    '''
     todo_list = get_object_or_404(ToDoList, pk=todo_list_id)
     template = loader.get_template('superlists/detail.html')
     form = ToDoListItemAdditionForm()
@@ -32,6 +50,15 @@ def detail(request, todo_list_id):
 
 
 def create_todo_list(request):
+    '''
+    Handles HTTP request to create new list.
+    If POST creates new list according to user input.
+    Args:
+        request - HTTP request
+
+    Returns:
+        Redirect to detailed view of newly created list if POST, 404 error code page otherwise.
+    '''
     if request.method == 'POST':
         form = ToDoListCreationForm(request.POST)
         if form.is_valid():
@@ -41,6 +68,16 @@ def create_todo_list(request):
         raise Http404("Resource does not exist")
 
 def add_todo_list_item(request, todo_list_id):
+    '''
+    Handles HTTP request to create new item in a list.
+    If POST creates new item on a list according to user input.
+    Args:
+        request - HTTP request
+        todo_list_id - id of a list corresponding to the new item
+
+    Returns:
+        Redirect to detailed view of corresponding list if POST, 404 error code page otherwise.
+    '''
     if request.method == 'POST':
         form = ToDoListCreationForm(request.POST)
         if form.is_valid():
