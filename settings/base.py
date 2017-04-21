1b2fb89d54e044091f4509d43849c2acbf8c3803
Nuurek <nurskijakub@gmail.com>
2017-04-21 23:25:54 +0200

    Cleaned INSTALLED_APPS setting.

--- settings/base.py
+++ settings/base.py
@@ -25,14 +25,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'superlists.apps.SuperlistsConfig',
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party apps
     'widget_tweaks',
+
+    # custom apps
+    'superlists.apps.SuperlistsConfig',
 ]
 
 MIDDLEWARE = [
--- superlists/apps.py
+++ superlists/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class SuperlistsConfig(AppConfig):
-    name = 'superlists'
+    name = "superlists"
+    verbose_name = "SuperLists"
