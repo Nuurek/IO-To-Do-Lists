d25554aecb1d0d480a0e0930b81f76ec80ee7ae8
Nuurek <nurskijakub@gmail.com>
2017-05-17 15:20:41 +0200

    Started accounts app. Moved UserProfile model from superlists to accounts.

--- settings/base.py
+++ settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     # custom apps
     'superlists.apps.SuperlistsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- superlists/models.py
+++ superlists/models.py
@@ -1,38 +1,7 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
-from django.conf import settings
 
-
-class UserProfile(models.Model):
-    """
-    Couples user with their's account confirmation code.
-
-    Attributes:
-        user - user owning the profile
-        confirmation_code - code used in account confirmation process
-    """
-    user = models.OneToOneField(User)
-    confirmation_code = models.CharField(blank=True, max_length=32)
-
-    def __str__(self):
-        return self.user.username
-
-    def get_absolute_url(self):
-        return reverse("user", kwargs={"user_id": self.user.pk})
-
-    def send_confirmation_code(self):
-        self.user.email_user("Superlists - Email Verification",
-                             "Please confirm your registration by clicking this link: "
-                             + "http://"
-                             + settings.ALLOWED_HOSTS[0]
-                             + reverse("register_confirm", kwargs={
-                                 "user_profile_id": self.id,
-                                 "code": self.confirmation_code}))
-
-    def activate_user(self):
-        self.user.is_active = True
-        self.user.save()
+from accounts.models import UserProfile
 
 
 class ToDoList(models.Model):
