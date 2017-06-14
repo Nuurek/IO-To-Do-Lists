ef3f2578d3c83fd1e8af6b5d6afdef233db124ea
Nuurek <nurskijakub@gmail.com>
2017-06-14 18:45:22 +0200

    First 2 functional tests.

--- functional_tests/test_accounts.py
+++ functional_tests/test_accounts.py
@@ -1,7 +1,22 @@
 from .base import FunctionalTest
 
 
-class SignUpTest(FunctionalTest):
+class AccountsTest(FunctionalTest):
 
-    def test_sign_up_button_on_home_page(self):
+    def test_sign_up(self):
         self.browser.get(self.server_url)
+        self.browser.find_element_by_link_text('Sign up').click()
+        form = self.browser.find_element_by_id('signup-form')
+        form.find_element_by_name('username').send_keys('test_user')
+        form.find_element_by_name('email').send_keys('test@gmail.com')
+        form.find_element_by_name('password').send_keys('test_password')
+        form.find_element_by_name('confirm_password').send_keys('test_password')
+        form.find_element_by_id('signup-submit').click()
+        self.assertTrue('accounts/register/success' in self.browser.current_url)
+
+    def test_login(self):
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_name('username').send_keys('test_user')
+        self.browser.find_element_by_name('password').send_keys('test_password')
+        self.browser.find_element_by_id('login-submit').click()
+        self.assertEqual(self.browser.current_url, self.server_url + '/')
